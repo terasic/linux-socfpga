94d7fe8243687162d139dd6c6f8a770305fc9808
Xu Jun-Ji <craze429@gmail.com>
2017-04-27 11:57:44 +0800

    "ALSA SoC DE1-SoC_WM8731" module rename to "ALSA SoC SoCFPGA_WM8731"

--- sound/soc/socfpga/socfpga-wm8731.c
+++ sound/soc/socfpga/socfpga-wm8731.c
@@ -1,5 +1,5 @@
 /*
- * de1-soc-wm8731 -- SoC audio for Terasic DE1-SoC board
+ * socfpga-wm8731 -- SoC audio for Terasic DE1-SoC and DE10-Standard board
  * Author: B. Steinsbo <bsteinsbo@gmail.com>
  *
  * Based on sam9g20_wm8731 by
@@ -28,7 +28,7 @@
 
 //static unsigned int i2c_mux_gpio;
 
-static int de1soc_hw_params(struct snd_pcm_substream *substream,
+static int socfpga_hw_params(struct snd_pcm_substream *substream,
 	struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
@@ -54,7 +54,7 @@ static int de1soc_hw_params(struct snd_pcm_substream *substream,
 	return 0;
 }
 
-static void de1soc_shutdown(struct snd_pcm_substream *substream)
+static void socfpga_shutdown(struct snd_pcm_substream *substream)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *codec_dai = rtd->codec_dai;
@@ -69,16 +69,16 @@ static void de1soc_shutdown(struct snd_pcm_substream *substream)
 	}
 }
 
-static struct snd_soc_ops de1soc_ops = {
+static struct snd_soc_ops socfpga_ops = {
 	// .startup
-	.shutdown = de1soc_shutdown,
-	.hw_params = de1soc_hw_params,
+	.shutdown = socfpga_shutdown,
+	.hw_params = socfpga_hw_params,
 	// .hw_free
 	// .prepare
 	// .trigger
 };
 
-static const struct snd_soc_dapm_widget de1soc_dapm_widgets[] = {
+static const struct snd_soc_dapm_widget socfpga_dapm_widgets[] = {
 	SND_SOC_DAPM_HP("Headphone Jack", NULL),
 	SND_SOC_DAPM_MIC("Microphone Jack", NULL),
 	SND_SOC_DAPM_LINE("Line In Jack", NULL),
@@ -96,7 +96,7 @@ static const struct snd_soc_dapm_route intercon[] = {
 	{"Headphone Jack", NULL, "RHPOUT"},
 };
 
-static int de1soc_wm8731_init(struct snd_soc_pcm_runtime *rtd)
+static int socfpga_wm8731_init(struct snd_soc_pcm_runtime *rtd)
 {
 	struct snd_soc_dai *codec_dai = rtd->codec_dai;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
@@ -130,34 +130,34 @@ static int de1soc_wm8731_init(struct snd_soc_pcm_runtime *rtd)
 	return 0;
 }
 
-static struct snd_soc_dai_link de1soc_dai = {
+static struct snd_soc_dai_link socfpga_dai = {
 	.name = "WM8731",
 	.stream_name = "WM8731 PCM",
 	.cpu_dai_name = "ff200000.i2s",
 	.codec_dai_name = "wm8731-hifi",
-	.init = de1soc_wm8731_init,
-	.platform_name = "de1soc",
+	.init = socfpga_wm8731_init,
+	.platform_name = "socfpga",
 	.codec_name = "wm8731.0-001a",
-	.ops = &de1soc_ops,
+	.ops = &socfpga_ops,
 };
 
-static struct snd_soc_card snd_soc_de1soc = {
-	.name = "DE1SOC-WM8731",
+static struct snd_soc_card snd_soc_socfpga = {
+	.name = "socfpga-WM8731",
 	.owner = THIS_MODULE,
-	.dai_link = &de1soc_dai,
+	.dai_link = &socfpga_dai,
 	.num_links = 1,
 
-	.dapm_widgets = de1soc_dapm_widgets,
-	.num_dapm_widgets = ARRAY_SIZE(de1soc_dapm_widgets),
+	.dapm_widgets = socfpga_dapm_widgets,
+	.num_dapm_widgets = ARRAY_SIZE(socfpga_dapm_widgets),
 	.dapm_routes = intercon,
 	.num_dapm_routes = ARRAY_SIZE(intercon),
 };
 
-static int de1soc_audio_probe(struct platform_device *pdev)
+static int socfpga_audio_probe(struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
 	struct device_node *codec_np, *cpu_np;
-	struct snd_soc_card *card = &snd_soc_de1soc;
+	struct snd_soc_card *card = &snd_soc_socfpga;
 	int ret;
 
 	if (!np) {
@@ -166,8 +166,8 @@ static int de1soc_audio_probe(struct platform_device *pdev)
 
 	card->dev = &pdev->dev;
 
-	// I2C bus is muxed between HPS and FPGA. Set mux to HPS 
-	/*	
+	// I2C bus is muxed between HPS and FPGA. Set mux to HPS
+	/*
 	i2c_mux_gpio = of_get_named_gpio(np, "i2c-mux-gpio", 0);
 	if (gpio_is_valid(i2c_mux_gpio)) {
 		ret = devm_gpio_request_one(&pdev->dev,
@@ -183,24 +183,24 @@ static int de1soc_audio_probe(struct platform_device *pdev)
 	}
 	*/
 	/* Parse codec info */
-	de1soc_dai.codec_name = NULL;
+	socfpga_dai.codec_name = NULL;
 	codec_np = of_parse_phandle(np, "audio-codec", 0);
 	if (!codec_np) {
 		dev_err(&pdev->dev, "codec info missing\n");
 		return -EINVAL;
 	}
-	de1soc_dai.codec_of_node = codec_np;
+	socfpga_dai.codec_of_node = codec_np;
 
 	/* Parse dai and platform info */
-	de1soc_dai.cpu_dai_name = NULL;
-	de1soc_dai.platform_name = NULL;
+	socfpga_dai.cpu_dai_name = NULL;
+	socfpga_dai.platform_name = NULL;
 	cpu_np = of_parse_phandle(np, "i2s-controller", 0);
 	if (!cpu_np) {
 		dev_err(&pdev->dev, "dai and pcm info missing\n");
 		return -EINVAL;
 	}
-	de1soc_dai.cpu_of_node = cpu_np;
-	de1soc_dai.platform_of_node = cpu_np;
+	socfpga_dai.cpu_of_node = cpu_np;
+	socfpga_dai.platform_of_node = cpu_np;
 
 	of_node_put(codec_np);
 	of_node_put(cpu_np);
@@ -213,7 +213,7 @@ static int de1soc_audio_probe(struct platform_device *pdev)
 	return ret;
 }
 
-static int de1soc_audio_remove(struct platform_device *pdev)
+static int socfpga_audio_remove(struct platform_device *pdev)
 {
 	struct snd_soc_card *card = platform_get_drvdata(pdev);
 
@@ -225,25 +225,25 @@ static int de1soc_audio_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static const struct of_device_id de1soc_wm8731_dt_ids[] = {
-	{ .compatible = "opencores,de1soc-wm8731-audio", },
+static const struct of_device_id socfpga_wm8731_dt_ids[] = {
+	{ .compatible = "opencores,socfpga-wm8731-audio", },
 	{ }
 };
-MODULE_DEVICE_TABLE(of, de1soc_wm8731_dt_ids);
+MODULE_DEVICE_TABLE(of, socfpga_wm8731_dt_ids);
 
-static struct platform_driver de1soc_audio_driver = {
+static struct platform_driver socfpga_audio_driver = {
 	.driver = {
-		.name	= "de1soc-audio",
+		.name	= "socfpga-audio",
 		.owner	= THIS_MODULE,
-		.of_match_table = of_match_ptr(de1soc_wm8731_dt_ids),
+		.of_match_table = of_match_ptr(socfpga_wm8731_dt_ids),
 	},
-	.probe	= de1soc_audio_probe,
-	.remove	= de1soc_audio_remove,
+	.probe	= socfpga_audio_probe,
+	.remove	= socfpga_audio_remove,
 };
 
-module_platform_driver(de1soc_audio_driver);
+module_platform_driver(socfpga_audio_driver);
 
 /* Module information */
 MODULE_AUTHOR("Bjarne Steinsbo <bsteinsbo@gmail.com>");
-MODULE_DESCRIPTION("ALSA SoC DE1-SoC_WM8731");
+MODULE_DESCRIPTION("ALSA SoC SoCFPGA_WM8731");
 MODULE_LICENSE("GPL");
